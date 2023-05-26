851861f295b1d6b800a95bbe1b4900bc06994deb
Manu Rico <ricobeltranmanuel@gmail.com>
2023-05-26 08:08:35 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RickAndMortyDomain
+
+A description of this package.
---
+++ Sources/RickAndMortyDomain/RickAndMortyDomain.swift
@@ -0,0 +1,6 @@
+public struct RickAndMortyDomain {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
